361a6eb40ccfb0906079f29047c22ddb1b24131e
JuanCarlos987 <juancarlosjaen.jaeniglesias@gmail.com>
2022-02-28 12:12:15 +0100

    [ADD]-Moudlo para añadir tag personalizadas a las lineas de los pedidos

---
+++ gst_sale_line_tags/README.rst
@@ -0,0 +1,5 @@
+===================
+GST SALE LINE TAG
+===================
+
+This module add a new tag to the line of a sale, with a color, also include a secuence in line taht depend s of the product category.
---
+++ gst_sale_line_tags/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "Sale line Tag",
+    "version": "15.0.1.0.1",
+    "author": "Guadaltech Soluciones Tecnológicas, Juan Carlos Jaén Iglesias",
+    "website": "https://github.com/OCA/queue",
+    "license": "AGPL-3",
+    "category": "Sale",
+    "depends": [
+        "sale",
+        "sales_team",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/sale_order_line.xml",
+        "views/sale_order_line_tag.xml",
+        "views/product_category.xml",
+    ],
+}
