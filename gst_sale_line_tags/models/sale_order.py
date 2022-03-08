1945cbd10447cabd4cbb7f7c322b2c0b2c0c541b
JuanCarlos987 <juancarlosjaen.jaeniglesias@gmail.com>
2022-03-08 11:43:54 +0100

    [FIX]-TAG module

--- gst_sale_line_tags/models/sale_order.py
+++ gst_sale_line_tags/models/sale_order.py
@@ -3,9 +3,7 @@ from odoo import fields, models
 
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
-    _order = "order"
 
     state_tag_ids = fields.Many2many(
         "sale.order.line.tag", relation="line_ids", string="Line State"
     )
-    order = fields.Integer(related="product_id.categ_id.sequence")
