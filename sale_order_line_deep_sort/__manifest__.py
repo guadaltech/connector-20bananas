af56d9a4dddeb2179f8c71bddfa28e98b599159f
JuanCarlos987 <juancarlosjaen.jaeniglesias@gmail.com>
2022-03-08 11:09:45 +0100

    [ADD]-Modulo para ordenar lineas de pedidos

---
+++ sale_order_line_deep_sort/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2018 Tecnativa - Vicent Cubells <vicent.cubells@tecnativa.com>
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3
+
+{
+    "name": "Sale Order Line Deep Sort",
+    "version": "15.0.1.0.0",
+    "author": "Guadaltech Soluciones Tecnológicas, Juan Carlos Jaén Iglesias",
+    "license": "AGPL-3",
+    "category": "Sale Management",
+    "summary": "Sale Order Line Sort",
+    "depends": ["sale"],
+    "data": ["views/res_config_settings_views.xml", "views/sale_order_views.xml","views/product_category.xml",],
+    "installable": True,
+}
---
+++ sale_order_line_deep_sort/models/res_config_settings.py
@@ -0,0 +1,25 @@
+# Copyright 2018 Tecnativa - Vicent Cubells <vicent.cubells@tecnativa.com>
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3
+
+from odoo import api, fields, models
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = "res.config.settings"
+
+    so_line_order_default = fields.Selection(
+        related="company_id.default_so_line_order",
+        string="Line Order",
+        readonly=False,
+    )
+    so_line_direction_default = fields.Selection(
+        related="company_id.default_so_line_direction",
+        string="Sort Direction",
+        readonly=False,
+    )
+
+    @api.onchange("so_line_order_default")
+    def onchange_so_line_order_default(self):
+        """ Reset direction line order when user remove order field value """
+        if not self.so_line_order_default:
+            self.so_line_direction_default = False
